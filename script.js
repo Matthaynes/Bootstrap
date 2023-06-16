782a5a75afb9ef340b244b32c435446fd59971d0
MatthewH18 <13919480-MatthewH18@users.noreply.replit.com>
2023-06-16 18:14:29 +0000

    Charts

--- script.js
+++ script.js
@@ -1,20 +1,48 @@
-const ctx = document.getElementById('myChart');
+ 
+    // Doughnut Chart
+    var doughnutData = {
+      labels: ['Points', 'Assists', 'Rebounds'],
+      datasets: [{
+        data: [26.4, 8.3, 10.8],
+        backgroundColor: ['#FF6384', '#36A2EB', '#FFCE56'],
+        hoverBackgroundColor: ['#FF6384', '#36A2EB', '#FFCE56']
+      }]
+    };
 
-new Chart(ctx, {
-  type: 'bar',
-  data: {
-    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-    datasets: [{
-      label: '# of Votes',
-      data: [12, 19, 3, 5, 2, 3],
-      borderWidth: 1
-    }]
-  },
-  options: {
-    scales: {
-      y: {
-        beginAtZero: true
+    var doughnutOptions = {
+      responsive: true
+    };
+
+    var doughnutChart = new Chart(document.getElementById('doughnutChart'), {
+      type: 'doughnut',
+      data: doughnutData,
+      options: doughnutOptions
+    });
+
+    // Bar Chart
+    var barData = {
+      labels: ['Steals', 'Blocks'],
+      datasets: [{
+        label: 'Nikola Jokić',
+        data: [1.3, 0.6],
+        backgroundColor: ['rgba(255, 159, 64, 0.5)', 'rgba(153, 102, 255, 0.5)'],
+        borderColor: ['rgba(255, 159, 64, 1)', 'rgba(153, 102, 255, 1)'],
+        borderWidth: 1
+      }]
+    };
+
+    var barOptions = {
+      responsive: true,
+      scales: {
+        y: {
+          beginAtZero: true
+        }
       }
-    }
-  }
-});
+    };
+
+    var barChart = new Chart(document.getElementById('barChart'), {
+      type: 'bar',
+      data: barData,
+      options: barOptions
+    });
+  </script>
